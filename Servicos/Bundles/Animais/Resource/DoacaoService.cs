5b6c928f78c998d5d10986f26be68b4ed99534f1
Rudson Mendes <rudsonm@edu.univali.br>
2018-04-17 23:42:30 -0300

    Emite notificação quando candidato é aceito para adoção.

--- Servicos/Bundles/Animais/Resource/DoacaoService.cs
+++ Servicos/Bundles/Animais/Resource/DoacaoService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using Servicos.Bundles.Core.Repository;
 using Servicos.Bundles.Pessoas.Entity;
+using System.Web.UI;
 
 namespace Servicos.Bundles.Animais.Resource
 {
@@ -25,16 +26,23 @@ namespace Servicos.Bundles.Animais.Resource
 
         public override void AfterUpdate(Doacao doacao)
         {
-            switch(doacao.Status)
-            {
-                case "CANCELADO":
-                    var emails = _repository
+            var candidatos = _repository
                             .GetAll<SolicitacaoAdocao>()
                             .Where(s => s.Doacao.Id == doacao.Id)
-                            .Select(s => s.Usuario.Email);
+                            .Select(s => new Pair(s.Usuario.Email, s.Status))
+                            .AsEnumerable();
+            var emails = candidatos
+                            .Where(p => !p.Second.Equals("ACEITO"))
+                            .Select(c => c.First.ToString());
+            switch (doacao.Status)
+            {
+                case "CANCELADO":                    
                     EnviarNotificacaoCancelamento(doacao, emails);
                 break;
                 case "FINALIZADO":
+                    var emailCandidatoAprovado = candidatos.First(c => c.Second.Equals("APROVADO")).First.ToString();
+                    EnviarNotificacaoNaoElegidos(doacao, emails);
+                    EnviarNotificacaoAprovado(doacao, emailCandidatoAprovado);
                 break;
             }
         }
@@ -42,24 +50,36 @@ namespace Servicos.Bundles.Animais.Resource
         private void EnviarNotificacaoCancelamento(Doacao doacao, IEnumerable<string> emails)
         {
             string mensagem = string.Concat(
-                        "A doação do(a) ",
-                        doacao.Animal.Especie,
-                        " ",
-                        doacao.Animal.Nome,
-                        " acaba de ser cancelada por seu responsável."
-                    );
+                "A doação do(a) ",
+                doacao.Animal.Especie,
+                " ",
+                doacao.Animal.Nome,
+                " acaba de ser cancelada por seu responsável."
+            );
             EnviadorEmail.EnviarMultiplos(emails, "Doação Cancelada.", mensagem);
         }
 
         private void EnviarNotificacaoNaoElegidos(Doacao doacao, IEnumerable<string> emails)
         {
             string mensagem = string.Concat(
-                        "Você não foi escolhido para adotar o(a)",
-                        doacao.Animal.Especie,
-                        " ",
-                        doacao.Animal.Nome
-                    );
+                "Você não foi escolhido para adotar o(a) ",
+                doacao.Animal.Especie,
+                " ",
+                doacao.Animal.Nome
+            );
             EnviadorEmail.EnviarMultiplos(emails, "Doação Finalizada.", mensagem);
+        }
+
+        private void EnviarNotificacaoAprovado(Doacao doacao, string email)
+        {
+            string mensagem = string.Concat(
+                "Parabéns, sua solicitação de adoção para o(a) ",
+                doacao.Animal.Especie,
+                " ",
+                doacao.Animal.Nome,
+                " acaba de ser aprovada!"
+            );
+            EnviadorEmail.Enviar(email, "Solicitação de Adoção Aprovada.", mensagem);
         }
     }
 }
